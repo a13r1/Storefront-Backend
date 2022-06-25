1ac09143c0f3d3a6c31d5191ddc8253ccea3b33c
a13r1 <ar7384057@gmail.com>
2022-06-25 19:17:37 +0200

    Update orders table in migrations

--- migrations/sqls/20220624014641-tables-up.sql
+++ migrations/sqls/20220624014641-tables-up.sql
@@ -1,20 +1,20 @@
-create table products(
+create table products (
     id serial primary key,
     name varchar(256) not null,
     price float(2) not null
 );
 
-create table users(
+create table users (
     id serial primary key,
     first_name varchar(256) not null,
     last_name varchar(256) not null,
     password varchar(256) not null
 );
 
-create table orders(
+create table orders (
     id serial primary key,
-    product_id int not null,
-    quantity int not null,
+    products_ids int [] not null,
+    products_quantities int [] not null,
     user_id int not null,
     status varchar(8) not null,
     foreign key(product_id) references products(id),
